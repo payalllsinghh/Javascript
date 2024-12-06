258a86d93d480461ec114dab42b3ac8c2b10f1bc
payalllsinghh <payal.s020597@gmail.com>
2024-12-06 21:33:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//ctrl + shift + j for opening console on browser
+
+let js = 'amazing';
+
+console.log(10 + 60 - 3); //will show up the calculation in console 
+
+let firstName = "Payal";
+console.log(firstName);
+
+let country = 'India';
+let continent = 'Asia';
+let population = 'in-billions';
+
+console.log("I belong to " + country + " which lies in " + continent + " has a popluation " + population);
